e6a9648b0281084968271474ad254d952097f01e
lahm <l292538769@gmail.com>
2019-02-24 17:40:45 +0800

    modify demo application.class code

--- app/src/main/java/com/lahm/easyprotector/TestApplication.java
+++ app/src/main/java/com/lahm/easyprotector/TestApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.lahm.library.VirtualCheckCallback;
 import com.lahm.library.EasyProtectorLib;
-import com.lahm.library.VirtualApkCheckUtil;
 
 /**
  * Project Name:EasyProtector
@@ -15,16 +14,12 @@ public class TestApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //AS模拟器的端口检测法会有问题，会抢localhost，所以这里加了个判断，平时真机可以直接调用端口检测
-        if (!EasyProtectorLib.checkIsRunningInEmulator())
-            VirtualApkCheckUtil.getSingleInstance().checkByPortListening(getPackageName(), new VirtualCheckCallback() {
-                @Override
-                public void findSuspect() {
-                    System.out.println("ceshi----");
-                    //为了防止callback造成的内存泄漏（比如传进了activity),检测成功并回调后就置空callback
-                    //个人认为，多次回调这个接口，没有意义，检测到广义多开后，杀掉克隆体就好
-//                    System.exit(0);
-                }
-            });
+//        可以在启动时创建localServerSocket
+//        EasyProtectorLib.checkIsRunningInVirtualApk(getPackageName(), new VirtualCheckCallback() {
+//            @Override
+//            public void findSuspect() {
+//                System.exit(0);
+//            }
+//        });
     }
 }
