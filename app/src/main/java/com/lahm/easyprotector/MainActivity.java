fe8e2279e59abef1bd425572d3cec5cc15fe9b74
lahm <l292538769@gmail.com>
2019-02-24 22:39:21 +0800

    recode demo

--- app/src/main/java/com/lahm/easyprotector/MainActivity.java
+++ app/src/main/java/com/lahm/easyprotector/MainActivity.java
@@ -1,139 +1,114 @@
 package com.lahm.easyprotector;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.TextView;
 
-import com.lahm.library.CommandUtil;
 import com.lahm.library.EasyProtectorLib;
 import com.lahm.library.EmulatorCheckCallback;
-import com.lahm.library.EmulatorCheckUtil;
 import com.lahm.library.SecurityCheckUtil;
 import com.lahm.library.VirtualApkCheckUtil;
 
-public class MainActivity extends AppCompatActivity {
-    EditText input;
-    Button output, clickEmulatorDetect;
-    TextView emulator;
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView one = findViewById(R.id.one);
-        one.setText(VirtualApkCheckUtil.getSingleInstance().checkByPrivateFilePath(this) ?
-                "privatePath-NO" : "privatePath-OK");
-        TextView two = findViewById(R.id.two);
-        two.setText(VirtualApkCheckUtil.getSingleInstance().checkByOriginApkPackageName(this) ?
-                "packageName-NO" : "packageName-OK");
-        TextView three = findViewById(R.id.three);
-        three.setText(VirtualApkCheckUtil.getSingleInstance().checkByMultiApkPackageName() ?
-                "maps-NO" : "maps-OK");
-        TextView four = findViewById(R.id.four);
-        four.setText(EasyProtectorLib.checkIsUsingMultiVirtualApp() ?
-                "process-NO" : "process-OK");
-
-        TextView root = findViewById(R.id.root);
-        root.setText(EasyProtectorLib.checkIsRoot() ?
-                "root" : "no-root");
-
-        TextView debug = findViewById(R.id.debug);
-        debug.setText(SecurityCheckUtil.getSingleInstance().checkIsDebugVersion(this) ?
-                "debug-version" : "release-version");
-
-        TextView xp = findViewById(R.id.xp);
-        xp.setText(EasyProtectorLib.checkIsXposedExist() ?
-                (EasyProtectorLib.checkXposedExistAndDisableIt() ?
-                        "has xp but closed" : "has xp but not close")
-                : "no-xp");
-
-        usb = findViewById(R.id.usb);
-        usb.setText(SecurityCheckUtil.getSingleInstance().checkIsDebuggerConnected() ?
-                "debugger-connect" : "no-debugger-connect");
-
-        TextView traced = findViewById(R.id.traced);
-        traced.setText(EasyProtectorLib.checkIsBeingTracedByJava() ?
-                "being traced" : "no-tracer");
-
-        final Button loadSO = findViewById(R.id.loadSO);
-        loadSO.setOnClickListener(v -> {
-            EasyProtectorLib.checkIsBeingTracedByC();
-            loadSO.setText("log中查看traceId");
-            loadSO.setClickable(false);
-        });
-
-        emulator = findViewById(R.id.emulator);
-        emulator.setText(EasyProtectorLib.checkIsRunningInEmulator(this) ? "isEmulator" : "not-emulator");
-        clickEmulatorDetect = findViewById(R.id.clickEmulatorDetect);
-        clickEmulatorDetect.setOnClickListener(v -> detecting());
-
-        input = findViewById(R.id.input);
-        output = findViewById(R.id.output);
-        output.setOnClickListener(v -> findPropertyName(input.getText().toString()));
+        findViewById(R.id.checkByPrivateFilePath).setOnClickListener(this);
+        findViewById(R.id.checkByOriginApkPackageName).setOnClickListener(this);
+        findViewById(R.id.checkByMultiApkPackageName).setOnClickListener(this);
+        findViewById(R.id.checkByHasSameUid).setOnClickListener(this);
+        findViewById(R.id.checkByPortListening).setOnClickListener(this);
+        findViewById(R.id.checkByCreateLocalServerSocket).setOnClickListener(this);
+        findViewById(R.id.checkRoot).setOnClickListener(this);
+        findViewById(R.id.checkDebuggable).setOnClickListener(this);
+        findViewById(R.id.checkDebuggerAttach).setOnClickListener(this);
+        findViewById(R.id.checkTracer).setOnClickListener(this);
+        findViewById(R.id.checkXP).setOnClickListener(this);
+        findViewById(R.id.readSysProperty).setOnClickListener(this);
+        findViewById(R.id.test).setOnClickListener(this);
     }
 
-    private void detecting() {
-        EmulatorCheckUtil.getSingleInstance().readSysProperty(this, new EmulatorCheckCallback() {
-            @Override
-            public void findEmulator(String emulatorInfo) {
-                emulator.setText(emulatorInfo);
-            }
-        });
+    private void forTest() {
+        //only for test
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_POWER_CONNECTED);
-        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
-        registerReceiver(receiver, intentFilter);
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(receiver);
-    }
-
-
-    private void findPropertyName(String propName) {
-        if (TextUtils.isEmpty(propName)) {
-            output.setText("property name cannot be null");
-            return;
-        } else {
-            output.setText(CommandUtil.getSingleInstance().getProperty(propName));
-        }
-    }
-
-    BatteryChangeBroadCastReceiver receiver = new BatteryChangeBroadCastReceiver();
-    private TextView usb;
-
-    //最好是开启子线程轮询的方式监听，在插上usb的一瞬间，debugger并没有attach上
-    private class BatteryChangeBroadCastReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            switch (action) {
-                case Intent.ACTION_POWER_CONNECTED:
-                    usb.setText(
-                            SecurityCheckUtil.getSingleInstance().checkIsUsbCharging(MainActivity.this) ?
-                                    SecurityCheckUtil.getSingleInstance().checkIsDebuggerConnected() ?
-                                            "debugger-connect！！" : "only-usb-charging"
-                                    : "only-charging");
-                    break;
-                case Intent.ACTION_POWER_DISCONNECTED:
-                    usb.setText("no-usb-connected");
-                    break;
-            }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.test:
+                forTest();
+                break;
+            case R.id.checkByPrivateFilePath:
+                TextView v1 = findViewById(R.id.v1);
+                v1.setText(VirtualApkCheckUtil.getSingleInstance().checkByPrivateFilePath(this, null) ?
+                        "privatePath-NO" : "privatePath-OK");
+                break;
+            case R.id.checkByOriginApkPackageName:
+                TextView v2 = findViewById(R.id.v2);
+                v2.setText(VirtualApkCheckUtil.getSingleInstance().checkByOriginApkPackageName(this, null) ?
+                        "packageName-NO" : "packageName-OK");
+                break;
+            case R.id.checkByMultiApkPackageName:
+                TextView v3 = findViewById(R.id.v3);
+                v3.setText(VirtualApkCheckUtil.getSingleInstance().checkByMultiApkPackageName(null) ?
+                        "maps-NO" : "maps-OK");
+                break;
+            case R.id.checkByHasSameUid:
+                TextView v4 = findViewById(R.id.v4);
+                v4.setText(VirtualApkCheckUtil.getSingleInstance().checkByHasSameUid(null) ?
+                        "uid-NO" : "uid-OK");
+                break;
+            case R.id.checkByPortListening:
+                VirtualApkCheckUtil.getSingleInstance().checkByPortListening("port", null);
+                TextView v5 = findViewById(R.id.v5);
+                v5.setText("port listening");
+                break;
+            case R.id.checkByCreateLocalServerSocket:
+                TextView v6 = findViewById(R.id.v6);
+                v6.setText(EasyProtectorLib.checkIsRunningInVirtualApk("lss", null) ?
+                        "LocalServerSocket-NO" : "LocalServerSocket-OK");
+                break;
+            case R.id.checkRoot:
+                TextView r1 = findViewById(R.id.r1);
+                r1.setText(EasyProtectorLib.checkIsRoot() ?
+                        "rooted" : "no-root");
+                break;
+            case R.id.checkDebuggable:
+                TextView d1 = findViewById(R.id.d1);
+                d1.setText(EasyProtectorLib.checkIsDebug(this) ?
+                        "debuggable" : "release");
+                break;
+            case R.id.checkDebuggerAttach:
+                TextView d2 = findViewById(R.id.d2);
+                d2.setText(SecurityCheckUtil.getSingleInstance().checkIsUsbCharging(MainActivity.this) ?
+                        SecurityCheckUtil.getSingleInstance().checkIsDebuggerConnected() ?
+                                "debugger-connect！！" : "only-usb-charging"
+                        : "only-charging");
+                break;
+            case R.id.checkTracer:
+                EasyProtectorLib.checkIsBeingTracedByC();
+                TextView d3 = findViewById(R.id.d3);
+                d3.setText("see log");
+                break;
+            case R.id.checkXP:
+                TextView x1 = findViewById(R.id.x1);
+                x1.setText(EasyProtectorLib.checkIsXposedExist() ?
+                        "shutdown xp success" : "failed");
+                break;
+            case R.id.readSysProperty:
+                TextView e1 = findViewById(R.id.e1);
+                EasyProtectorLib.checkIsRunningInEmulator(this, new EmulatorCheckCallback() {
+                    @Override
+                    public void findEmulator(String emulatorInfo) {
+                        e1.setText(emulatorInfo);
+                    }
+                });
+                break;
         }
     }
 }
